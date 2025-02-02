f9f7397e29f84eafd505947b6ff945f817a05622
sidraafaq-si <adnanafaqadnan7@gmail.com>
2025-02-02 07:50:25 -0800

    new changes

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,5 +1,5 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-21'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-02-02'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
